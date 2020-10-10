95e34a75860ea304a44e229eb8f23aed3159318f
nf261 <72325289+nf261@users.noreply.github.com>
2020-10-10 11:50:16 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -72,11 +72,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     quitCommand = 'QUIT\r\n'
     clientSocket.send(quitCommand.encode())
     recv5 = clientSocket.recv(1024).decode()
-    # print(recv5)
-    # if recv5[:3] != '221':
-        print('221 reply not received from server.')
     # Fill in end
-    # Fill in end
+   
 
 
 if __name__ == '__main__':
