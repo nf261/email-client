8ab0a80110520efa684211b1f23e75c17f952e98
nf261 <72325289+nf261@users.noreply.github.com>
2020-10-10 10:44:52 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -29,7 +29,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and print server response.
     # Fill in start
-    mailfromCommand = 'Mail From:<nasser.fattah@gmail.com>\r\n'
+    mailfromCommand = 'MAIL FROM:<nasser.fattah@gmail.com>\r\n'
     clientSocket.send(mailfromCommand.encode())
     recv2 = clientSocket.recv(1024).decode()
     # print(recv1)
